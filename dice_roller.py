a8c96f66ef38fa1e92762be486b6d1dce5d71b9c
stokezs <gcstokesanu@gmail.com>
2020-08-24 17:47:11 +1000

    change

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,11 @@
 def main():
     import random
+    dice_sum = 0
     dice_rolls = 2
     for i in range(0,dice_rolls):
     roll=random.randint(1,6)
-    print(f'You rolled a {roll}')
+    dice_sum = dice_sum + roll
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
     main()
