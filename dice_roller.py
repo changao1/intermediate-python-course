f04a11df7faf85fd05962a549aa27827e06236e3
stokezs <gcstokesanu@gmail.com>
2020-08-24 17:50:21 +1000

    5

--- dice_roller.py
+++ dice_roller.py
@@ -3,7 +3,7 @@ def main():
     dice_sum = 0
     dice_rolls = 2
     for i in range(0,dice_rolls):
-    roll=random.randint(1,6)
+    roll = random.randint(1,6)
     dice_sum = dice_sum + roll
     print(f'You have rolled a total of {dice_sum}')
 
